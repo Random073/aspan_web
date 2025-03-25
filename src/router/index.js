27aab07517d2d0574e0f8d05c69c0dce62c1da69
Random073 <nurkhan@portal.local>
2025-03-25 13:36:37 +0500

    added webhashhistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "/src/components/Home.vue";
 import IslandContainer from "/src/components/IslandContainer.vue";
 import Microlessons from "/src/components/Microlessons.vue";
@@ -25,7 +25,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory("/aspan_web"),
+  history: createWebHashHistory("/aspan_web"),
   routes,
 });
 
